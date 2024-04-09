ad6454f841b0c2804bd86b21ae48c54bfe71d79a
raky <raky@daum.net>
2024-04-09 17:04:43 +0900

    .gitignore add, fastapi docs endpoint change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import List
 from pydantic import BaseModel
 import sqlite_utils
 
-app = FastAPI()
+app = FastAPI(docs_url='/api/docs', openapi_url='/api/openapi.json')
 
 # SQLite 데이터베이스 연결
 db = sqlite_utils.Database("/data/data.db")
@@ -14,13 +14,13 @@ class Item(BaseModel):
     description: str = None
 
 # 데이터베이스에 존재하는 모든 아이템을 반환하는 엔드포인트
-@app.get("/items/", response_model=List[Item])
+@app.get("/api/items/", response_model=List[Item])
 async def read_items():
     items = db["items"].rows
     return items
 
 # 데이터베이스에 새로운 아이템을 추가하는 엔드포인트
-@app.post("/items/")
+@app.post("/api/items/")
 async def create_item(item: Item):
     item_dict = item.dict()
     db["items"].insert(item_dict)
